a81adad907dd3f9c0c17cef42c15ee92f18453ac
dasom <dev.somda@gmail.com>
2025-07-21 18:00:25 +0900

    feat(fe): 생성한 일정 목록 조회 api 연결

--- manna-frontend/src/app/mypage/[id]/page.tsx
+++ manna-frontend/src/app/mypage/[id]/page.tsx
@@ -4,36 +4,43 @@ import styles from "./page.module.css";
 import { FaCircleUser } from "react-icons/fa6";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
-const TEMP_ROOM = [
-  {
-    name: "7월 프로젝트 회의",
-    start_date: "2025-07-01",
-    end_date: "2025-07-09",
-    description: "다음 단계 논의",
-    participant: 3,
-  },
-  {
-    name: "8월 프로젝트 회의",
-    start_date: "2025-07-01",
-    end_date: "2025-07-09",
-    description: "다음 단계 논의",
-    participant: 0,
-  },
-  {
-    name: "신규 채용",
-    start_date: "2025-07-01",
-    end_date: "2025-07-09",
-    description: "다음 단계 논의",
-    participant: 2,
-  },
-];
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ScheduleType } from "@/types/schedule";
+import { FaUsers } from "react-icons/fa6";
 
 export default function MyPage() {
   const router = useRouter();
   const { data } = useSession();
 
-  const signout = () => {
+  const [scheduleList, setScheduleList] = useState<ScheduleType[]>([]);
+
+  const getSchedules = async () => {
+    try {
+      const token = localStorage.getItem("accessToken");
+
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/schedules`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(res);
+      setScheduleList(res.data.schedules);
+    } catch (error) {
+      console.error("응답 제출 실패", error);
+      alert("일정 조회에 실패했습니다.");
+    }
+  };
+
+  useEffect(() => {
+    getSchedules();
+  }, []);
+
+  /** 로그아웃 */
+  const logout = () => {
     signOut({ redirect: false }).then(() => {
       router.replace("/");
     });
@@ -51,25 +58,37 @@ export default function MyPage() {
             <p>{data?.user?.nickname ?? "닉네임 미지정"}</p>
           </div>
         </div>
-        <button className={styles.userInfoRightButton}>수정하기</button>
+        <button className={styles.userInfoRightButton} onClick={logout}>
+          로그아웃
+        </button>
       </div>
 
       <div className={styles.roomListContainer}>
         <h3>생성한 일정</h3>
         <div className={styles.roomList}>
-          {TEMP_ROOM.map((room, idx) => (
-            <button key={idx} className={styles.roomInfoButton}>
-              <h4>{room.name}</h4>
-              <p>
-                {room.start_date} - {room.end_date}
-              </p>
-              <p>{room.description}</p>
-              <p>참여자: {room.participant}명</p>
-            </button>
-          ))}
+          {scheduleList && scheduleList.length > 0 ? (
+            scheduleList.map((schedule, idx) => (
+              <button
+                key={`${schedule.code}_${idx}`}
+                className={styles.roomInfoButton}
+              >
+                <h4>{schedule.name}</h4>
+                <p>
+                  일정: {schedule.start_date.split(" ")[0]} -{" "}
+                  {schedule.end_date.split(" ")[0]}
+                </p>
+                <p>{schedule.description}</p>
+                <div>
+                  <FaUsers />
+                  <span>응답: {schedule.schedule_participants.length}명</span>
+                </div>
+              </button>
+            ))
+          ) : (
+            <>생성한 일정이 없습니다.</>
+          )}
         </div>
       </div>
-      <button onClick={signout}>임시 로그아웃</button>
     </div>
   );
 }
