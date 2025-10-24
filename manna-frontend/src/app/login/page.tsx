63a6fb43ad2266589ca1946af5d2aef6888bf1d9
dasom <dev.somda@gmail.com>
2025-10-24 16:13:05 +0900

    feat(fe): 로그인 페이지 추가

--- manna-frontend/src/app/login/page.tsx
+++ manna-frontend/src/app/login/page.tsx
@@ -1,77 +1,15 @@
-"use client";
-
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import axios from "axios";
 import Header from "@/components/home/Header";
-import { useToast } from "../_components/ToastProvider";
-import clientApi from "../api/client";
 import Gap from "@/components/base/Gap";
 
 export default function Login() {
-  const router = useRouter();
-  const { showToast } = useToast();
-
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleSubmit = async () => {
-    try {
-      signIn("credentials", {
-        email: form.email,
-        password: form.password,
-        redirect: false,
-      });
-      const res = await clientApi.post(`/login`, form, {
-        headers: { skipAuth: true },
-      });
-
-      const token = res.data.access_token;
-      if (token) localStorage.setItem("accessToken", token);
-
-      showToast("로그인 되었습니다!", "success");
-      router.replace("/home");
-    } catch (err: unknown) {
-      if (axios.isAxiosError(err)) {
-        console.error("Axios error:", err.response);
-        showToast(
-          err.response?.data?.message ?? "로그인 실패: 서버 오류입니다.",
-          "error"
-        );
-      } else {
-        console.error("Unexpected error:", err);
-        showToast("예상치 못한 오류가 발생했습니다.", "error");
-      }
-    }
-  };
-
-  const handleKakaoLogin = () => {
-    // 백엔드 OAuth 엔드포인트로 직접 리디렉션
-    window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/login/kakao`;
-  };
-
-  const handleGoogleLogin = () => {
-    // 백엔드 OAuth 엔드포인트로 직접 리디렉션
-    window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/login/google`;
-  };
-
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Header title="로그인" showBackButton />
 
-      <Gap direction="col" gap={76} className="mx-16">
-        <Gap direction="col" gap={40} width="full" className="mt-72">
+      <Gap direction="col" gap={76}>
+        <Gap direction="col" gap={40} width="full" className="mt-52 ">
           <Gap direction="col" gap={20}>
             <Image src="/logo_light.svg" alt="logo" width={100} height={100} />
             <p className="text-center text-head24 text-gray-900">
@@ -79,9 +17,9 @@ export default function Login() {
             </p>
           </Gap>
 
-          <Gap direction="col" gap={20} width="full">
-            <button
-              onClick={handleKakaoLogin}
+          <Gap direction="col" gap={8} width="full">
+            <Link
+              href={`${process.env.NEXT_PUBLIC_BASE_URL}/login/kakao`}
               className="relative w-full h-56 flex items-center justify-center bg-[#FEE500] hover:bg-[#FDD835] text-body16 text-gray-900 rounded-full transition"
             >
               <Image
@@ -92,11 +30,11 @@ export default function Login() {
                 className="absolute left-20"
               />
               카카오로 시작하기
-            </button>
+            </Link>
 
-            <button
-              onClick={handleGoogleLogin}
-              className="relative w-full h-56 flex items-center justify-center gap-2 border border-gray-300 text-body16 text-gray-900 rounded-full hover:bg-gray-50 transition"
+            <Link
+              href={`${process.env.NEXT_PUBLIC_BASE_URL}/login/google`}
+              className="relative w-full h-56 flex items-center justify-center gap-2 border border-gray-200 text-body16 text-gray-900 rounded-full hover:bg-gray-50 transition"
             >
               <Image
                 src="/icons/google.svg"
@@ -106,16 +44,16 @@ export default function Login() {
                 className="absolute left-20"
               />
               Google로 시작하기
-            </button>
+            </Link>
           </Gap>
         </Gap>
 
         <div className="flex items-center justify-center gap-30 text-body14 text-gray-800">
-          <Link href="/login/email" className="hover:underline">
+          <Link href="/login/email" className="w-90 text-center">
             이메일 로그인
           </Link>
-          <span className="w-px h-12 bg-gray-300" />
-          <Link href="/signup" className="hover:underline">
+          <span className="w-px h-20 bg-gray-200" />
+          <Link href="/signup" className="w-90 text-center">
             이메일 회원가입
           </Link>
         </div>
