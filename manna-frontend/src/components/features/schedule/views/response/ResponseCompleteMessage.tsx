0a4de787ac5489e67c37dc73290dc40ded134de1
dasom <dev.somda@gmail.com>
2026-02-24 11:54:45 +0900

    chore(fe): 확정 후 안내 문구 수정

--- manna-frontend/src/components/features/schedule/views/response/ResponseCompleteMessage.tsx
+++ manna-frontend/src/components/features/schedule/views/response/ResponseCompleteMessage.tsx
@@ -11,10 +11,7 @@ export default function ResponseCompleteMessage() {
       </div>
       <h3 className="text-head26 text-black mb-4">일정 답변 완료!</h3>
       <p className="text-body14 text-gray-900">잠시만 기다려주세요</p>
-      <p className="text-body14 text-gray-900">
-        일정이 확정되면 입력하신 연락처로 안내드릴게요
-      </p>
+      <p className="text-body14 text-gray-900">일정이 확정되면 입력하신 메일로 안내해드릴게요</p>
     </div>
   );
 }
-
